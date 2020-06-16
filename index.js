ff6dedefb2514a2141b7602e95aa925fb9354058
Luka <luka.levac@eestec-lj.org>
2020-06-16 18:50:26 +0200

    'com'

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 'use strict'
 
-// Libp2p Core
-const Libp2p = require('libp2p')
-// Transports
-const TCP = require('libp2p-tcp')
-const Websockets = require('libp2p-websockets')
 const WebrtcStar = require('libp2p-webrtc-star')
-// wrtc for node to supplement WebrtcStar
-const wrtc = require('wrtc')
-// Signaling Server for webrtc
+
 const SignalingServer = require('libp2p-webrtc-star/src/sig-server')
 
 ;(async () => {
 
   const signalingServer = await SignalingServer.start({
+    port: 8080
   })
 
   const ssAddr = `/ip4/${signalingServer.info.host}/tcp/${signalingServer.info.port}/ws/p2p-webrtc-star`
